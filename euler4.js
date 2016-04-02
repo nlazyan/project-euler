e62a4a467a9e9a45074fc2047f75b924fdb15da2
nlazyan <nonalaz@gmail.com>
2016-04-02 12:59:59 +0400

    project euler javascript solution

---
+++ euler4.js
@@ -0,0 +1,22 @@
+
+	//Largest palindrome product
+function rev(num){
+	return num.toString().split("").reverse().join("");
+} 
+	// // console.log(rev(256));
+var result=1;
+var revArr = [];
+for (var i = 999; i>100; i--) {
+for (var j = 999; j>100; j--) {
+	result = i*j;
+	if(rev(result)==result)
+	 	revArr.push(result);   	
+	};
+};
+function arrayMax(arr) {
+  return arr.reduce(function (p, v) {
+    return ( p > v ? p : v );
+  });
+}
+
+console.log(arrayMax(revArr));
